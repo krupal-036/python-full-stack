09c6a3e355bf4c2128b548c4a63ff21fe3f40c5e
Krupal Fataniya <krupalfataniya007@gmail.com>
2026-05-13 01:03:04 +0530

    Use environment variable for Flask debug mode
    Update Flask app run configuration to use environment variable for debug mode.

--- backend/index.py
+++ backend/index.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from routes.user_routes import user_bp
 import os
 
+load_dotenv()
 app = Flask(__name__, static_folder='public', static_url_path='/')
 CORS(app)
 
@@ -31,4 +32,4 @@ if __name__ == "__main__":
     if not os.path.exists(app.static_folder):
         os.makedirs(app.static_folder)
         
-    app.run(debug=True, port=5000)
+    app.run(debug=os.getenv("FLASK_DEBUG", "false").lower() == "true", port=5000)
